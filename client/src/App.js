417fbdfebd75b4d1313eb49b3f1bc64f5c75fb6d
Khiet <khiet.vo@urbanise.com>
2023-12-13 09:36:48 +0700

    add(client): send number to server by socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import io from 'socket.io-client';
 const hostWs = 'http://localhost:3001/';
+const LIMIT_NUMBER = 100_000;
 
 const socket = io.connect(hostWs);
 
 function App() {
+    function validateInput(input) {
+        try {
+            input = parseInt(input);
+        } catch (error) {
+            return false;
+        }
+        return Number.isInteger(input) && input >= 0 && input <= LIMIT_NUMBER;
+    }
+    function onSubmit(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const value = e.target.input.value;
+        if (validateInput(value)) {
+            socket.emit('sendNumber', { value });
+        }
+    }
     return (
         <div className="App">
             <h1 className="title">Hello React ft. Million</h1>
