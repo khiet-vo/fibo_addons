0ec27936c3fbf703f68407dfbd2df92401fdf29b
Khiet <khiet.vo@urbanise.com>
2023-12-13 09:33:26 +0700

    add(client): add simple form and socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import io from 'socket.io-client';
+const hostWs = 'http://localhost:3001/';
+
+const socket = io.connect(hostWs);
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="title">Hello React ft. Million</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="title">Hello React ft. Million</h1>
+            <div className="content">
+                <form className="formWrapper" onSubmit={onSubmit}>
+                    <input type="number" name="input" />
+                    <button type="submit">Send Number</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default App;
