dab2e6b6cc4cb45ee1922a177c14e3e6a9c78932
Khiet <khiet.vo@urbanise.com>
2023-12-13 13:13:56 +0700

    add(backend): gmp from boost::multiprecision

--- addons/addons.cc
+++ addons/addons.cc
@@ -1,7 +1,14 @@
-// build simple addon
 #include <napi.h>
 
+#include <boost/multiprecision/gmp.hpp>
+
 using namespace Napi;
+using namespace boost::multiprecision;
+
+String mpzIntToNapiString(const Env& env, const mpz_int& mpzInt) {
+  std::string stdStr = mpzInt.str();
+  return String::New(env, stdStr);
+}
 
 Value CallEmitFibo(const CallbackInfo& info) {
   Env env = info.Env();
@@ -18,14 +25,15 @@ Value CallEmitFibo(const CallbackInfo& info) {
   int count = info[0].As<Number>().Int32Value();
 
   emit.Call({String::New(env, "start")});
-  unsigned long long tmp, a = 0, b = 1;
-  
+
+  mpz_int tmp, a = 0, b = 1;
+
   //   int start = clock();
   for (int i = 0; i < count; i++) {
     tmp = a;
     a = b;
     b = tmp + a;
-    emit.Call({String::New(env, "data"), String::New(env, std::to_string(a))});
+    emit.Call({String::New(env, "data"), mpzIntToNapiString(env, a)});
   }
   //   int end = clock();
   //   std::cout << "it took " << end - start << "ticks, or "
@@ -42,5 +50,12 @@ Object Init(Env env, Object exports) {
 
   return exports;
 }
+
+// function use to validate the number of fibonacci nth
+// mpz_int fibonacciNTH(int n) {
+//   const mpf_float phi = (1 + sqrt(mpf_float(5))) / 2;
+//   mpf_float fib = floor(pow(phi, n) / sqrt(mpf_float(5)) + mpf_float(0.5));
+//   return fib.convert_to<mpz_int>();
+// }
 
 NODE_API_MODULE(addons, Init)
--- binding.gyp
+++ binding.gyp
@@ -9,6 +9,7 @@
             "dependencies": ["<!(node -p \"require('node-addon-api').gyp\")"],
             "cflags!": ["-fno-exceptions"],
             "cflags_cc!": ["-fno-exceptions"],
+            "libraries": ["-lgmp"],
             "xcode_settings": {
                 "GCC_ENABLE_CPP_EXCEPTIONS": "YES",
                 "CLANG_CXX_LIBRARY": "libc++",
