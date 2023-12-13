9127d722bcb949ec1e312bfd2d281adb64c716f9
Khiet <khiet.vo@urbanise.com>
2023-12-13 13:00:08 +0700

    add(backend): fibo with streaming data to worker

--- addons/addons.cc
+++ addons/addons.cc
@@ -1,23 +1,44 @@
 // build simple addon
 #include <napi.h>
 
-namespace helloFunction {
-std::string hello();
-Napi::String HelloWrapped(const Napi::CallbackInfo& info);
-Napi::Object Init(Napi::Env env, Napi::Object exports);
-}  // namespace helloFunction
+using namespace Napi;
 
-std::string helloFunction::hello() { return "Hello World"; }
+Value CallEmitFibo(const CallbackInfo& info) {
+  Env env = info.Env();
 
-Napi::String helloFunction::HelloWrapped(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::String returnValue = Napi::String::New(env, helloFunction::hello());
+  if (info.Length() < 2) {
+    throw TypeError::New(env, "Expected two arguments");
+  } else if (!info[0].IsNumber()) {
+    throw TypeError::New(env, "Expected first arg to be number");
+  } else if (!info[1].IsFunction()) {
+    throw TypeError::New(env, "Expected second arg to be function");
+  }
 
-  return returnValue;
+  Function emit = info[1].As<Function>();
+  int count = info[0].As<Number>().Int32Value();
+
+  emit.Call({String::New(env, "start")});
+  unsigned long long tmp, a = 0, b = 1;
+  
+  //   int start = clock();
+  for (int i = 0; i < count; i++) {
+    tmp = a;
+    a = b;
+    b = tmp + a;
+    emit.Call({String::New(env, "data"), String::New(env, std::to_string(a))});
+  }
+  //   int end = clock();
+  //   std::cout << "it took " << end - start << "ticks, or "
+  //             << ((float)end - start) / CLOCKS_PER_SEC << "seconds." <<
+  //             std::endl;
+
+  emit.Call({String::New(env, "end")});
+  return String::New(env, "OK");
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-  exports.Set("hello", Napi::Function::New(env, helloFunction::HelloWrapped));
+Object Init(Env env, Object exports) {
+  exports.Set(String::New(env, "callEmitFibo"),
+              Function::New(env, CallEmitFibo));
 
   return exports;
 }
--- worker.js
+++ worker.js
@@ -1,4 +1,17 @@
 const { parentPort, workerData } = require('worker_threads');
-const { hello } = require('bindings')('addonsFibo');
-console.log('🚀 ~ file: worker.js:3 ~ addonsFibo:', hello());
-parentPort.postMessage('workerdata' + workerData);
+const EventEmitter = require('events').EventEmitter;
+const { callEmitFibo } = require('bindings')('addonsFibo');
+
+const emitter = new EventEmitter();
+
+emitter.on('start', () => {
+    console.log('### START ...');
+});
+emitter.on('data', (evt) => {
+    parentPort.postMessage(evt);
+});
+emitter.on('end', () => {
+    console.log('🚀 ~ file: worker.js:15 ~ emitter.on ~ end:');
+});
+
+callEmitFibo(workerData, emitter.emit.bind(emitter));
